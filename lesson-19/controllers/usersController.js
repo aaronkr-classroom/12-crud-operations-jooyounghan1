8af65d78360a3de8187d18bc0ba32f1599c4eabb
jooyounghan1 <163230749+jooyounghan1@users.noreply.github.com>
2024-05-29 05:43:48 +0000

    19 & 20끝

--- lesson-19/controllers/usersController.js
+++ lesson-19/controllers/usersController.js
@@ -37,10 +37,37 @@ module.exports = {
    * userController.js에 액션 생성 추가
    */
   // 폼의 렌더링을 위한 새로운 액션 추가
-  /**
-   * @TODO: new, create, redirectView 액션을 객체 리터럴로 묶어 익스포트
-   */
-
+  new: (req, res) => {
+    res.render("users/new");
+  },
+  create: (req, res, next) => {
+    let userParams = {name: 
+    {
+      first: req.body.first,
+      last: req.body.last,
+    },
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password,
+    profileImg: req.body.profileImg,
+    };
+    User.create(userParams)
+    .then((user) => {
+      console.log(`Created user: ${user.name}`);
+      res.locals.redirect = "/users";
+      res.locals.user = user;
+      next();
+    })
+    .catch(error => {
+      console.log(`Error creating user: ${error.message}`);
+      next(error);
+    });
+  },
+  redirectView: (req, res, next) => {
+    let path = res.locals.redirect;
+    if(path) res.redirect(path);
+    else next();
+  },
   /**
    * 노트: 구독자 컨트롤러에 new와 create 액션을 추가하는 것은 새로운 CRUD 액션을 맞춰
    * getAllSubscribers와 saveSubscriber 액션을 삭제할 수 있다는 의미다. 게다가 홈
@@ -54,4 +81,19 @@ module.exports = {
   /**
    * @TODO: show, showView 액션을 객체 리터럴로 묶어 익스포트
    */
+  show: (req, res, next) => {
+    let userId = req.params.id; //URL 매개변수 (/users/:id)
+    User.findById(userId)
+    .then(user => {
+      res.locals.user = user;
+      next();
+    })
+    .catch(error => {
+      console.log(`Error fetching user: ${error.message}`);
+      next(error);
+    });
+  },
+  showView: (req, res) => {
+    res.render("users/show");
+  }
 };
--- lesson-19/models/User.js
+++ lesson-19/models/User.js
@@ -1,6 +1,8 @@
 // models/User.js
 "use strict";
 
+const Subscriber = require("./Subscriber");
+
 /**
  * Listing 18.1 (p. 259)
  * user.js에서 사용자 모델 생성
@@ -80,6 +82,24 @@ module.exports = mongoose.model("User", userSchema);
  * Listing 19.4 (p. 281)
  * user.js에 pre("save") 훅 추가
  */
+userSchema.pre("save", (next) => {
+  let user = this;
+  if (user.subscribedAccount == undefined){
+    Subscriber.findOne({
+      email: user.email
+    })
+    .then(Subscriber => {
+      user.subscribedAccount = subscriber;
+      next();
+    })
+    .catch(error => {
+      console.log(`Error in connecting subscriber: ${error.message}`);
+      next(error);
+    });
+  } else{
+    next();
+  }
+});
 /**
  * @TODO: pre("save") 훅 설정
  */
